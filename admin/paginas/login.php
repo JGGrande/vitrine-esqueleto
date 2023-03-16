2e052cc24ebd650a5d0cea1e4d1aba5e26e2bde0
JGGrande <joaogabriel7303@gmail.com>
2023-03-15 22:14:22 -0300

    Mudança no estilo de login

--- admin/paginas/login.php
+++ admin/paginas/login.php
@@ -11,9 +11,15 @@
         $consulta = $pdo->prepare($sql);
         $consulta->bindParam(":login", $login);       
         $consulta->execute();
+
         $dados = $consulta->fetch(PDO::FETCH_OBJ);
  
-        var_dump($dados);
+        if(!isset($dados->id)){
+            mensagemErro("Usuario não encontrado ou inativo.");
+        }
+        if(!isset($dados->senha)){
+            mensagemErro("Senha invalida!");
+        }
         
         //echo $login;
     }
@@ -22,8 +28,16 @@
 ?>
 
 
-<div class="login rounded bg-light" style="margin-top: 20vh;">
-    <h1 class="text-center ">Efetuar Login</h1>
+<div class="login rounded " style="
+margin-top: 20vh; 
+background: rgba(255, 255, 255, 0.43);
+box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+backdrop-filter: blur(5.2px);
+-webkit-backdrop-filter: blur(5.2px);
+border: 1px solid rgba(255, 255, 255, 0.1);
+">
+
+    <h1 class="text-center text-light">Efetuar Login</h1>
     <form  method="POST">
         <label for="login" class="">Login</label>
         <input type="text" name="login" id="login" class="form-control" required placeholder="Porfavor digite o usuario">
